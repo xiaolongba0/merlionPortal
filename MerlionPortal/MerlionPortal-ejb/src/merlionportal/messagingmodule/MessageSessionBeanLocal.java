d4fd731d26833e015e6ef4d9c426e46279bd2eee
xiaolongba0 <xiaolongba0@users.noreply.github.com>
2014-09-05 18:20:49 +0800

    changed package name -- manli

--- MerlionPortal/MerlionPortal-ejb/src/merlionportal/messagingmodule/MessageSessionBeanLocal.java
+++
@@ -1,8 +0,0 @@
-package merlionportal.messagingmodule;
-
-import javax.ejb.Local;
-
-@Local
-public interface MessageSessionBeanLocal {
-    public void createMessage(String title, String re, String message);
-}
