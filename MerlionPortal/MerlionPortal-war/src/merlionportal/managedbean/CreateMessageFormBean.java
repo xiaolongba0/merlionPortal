707e8765a465f5bd25036304ae126686241dcef7
xiaolongba0 <xiaolongba0@users.noreply.github.com>
2014-09-05 18:23:41 +0800

    changed package name -- manli

--- MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/CreateMessageFormBean.java
+++ MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/CreateMessageFormBean.java
@@ -4,7 +4,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import merlionportal.messagingmodule.MessageSessionBean;
+import merlionportal.ci.messagingmodule.MessageSessionBean;
 
 @ManagedBean
 @RequestScoped
