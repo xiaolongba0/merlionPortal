c2c30973fbfb37f66b4cc9fec1fe67b7b5899efc
xiaolongba0 <xiaolongba0@users.noreply.github.com>
2014-09-11 20:36:05 +0800

    changed bean methods - Manli

---
+++ MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/UserAccountManagerBean.java
@@ -0,0 +1,239 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package merlionportal.managedbean;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import merlionportal.ci.administrationmodule.UserAccountManagementBean;
+import merlionportal.utility.MD5Generator;
+
+/**
+ *
+ * @author manliqi
+ */
+@Named(value = "userAccountManagedBean")
+@ViewScoped
+public class UserAccountManagerBean {
+
+    /**
+     * Creates a new instance of UserAccountManagerBean
+     */
+    @EJB
+    private UserAccountManagementBean uamb;
+
+    private FacesContext ctx;
+
+    private Integer userId;
+    private Integer companyId;
+    private String companyName;
+    private String companyAddress;
+    private String contactNumber;
+    private String contactPersonName;
+    private String emailAddress;
+    private String description;
+    private Integer package1;
+
+    private String firstNameNewUser;
+    private String lastNameNewUser;
+    private String emailAddressNewUser;
+    private String passwordNewUser;
+    private String postalAddressNewUser;
+    private String contactNumberNewUser;
+    private String salutionNewUser;
+    private String userTypeNewUser;
+    private Integer companyIdNewUser;
+
+    
+
+    public UserAccountManagerBean() {
+
+        
+    }
+
+    public void createSystemAdmin(ActionEvent event) {
+
+//        Integer operatorId = (Integer) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userId");
+        Integer operatorId = 2;
+        if (operatorId != null) {
+            String hashedPass = MD5Generator.hash(passwordNewUser);
+            int result = uamb.createCompanySystemAdminUser(operatorId, firstNameNewUser, lastNameNewUser, emailAddressNewUser, hashedPass, postalAddressNewUser, contactNumberNewUser, salutionNewUser, userTypeNewUser, companyIdNewUser);
+            if (result == -1) {
+                System.out.println("Access Denied");
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Access Denied!", "You do not have sufficient right to perform this action!"));
+            } else if (result == 1) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "New System Admin Added!", ""));
+
+            } else {
+                // direct to login page
+            }
+
+        }
+
+    }
+
+    
+    public void registerNewCompany(ActionEvent event) {
+
+        int result = uamb.registerNewCompany(companyName, emailAddress, contactNumber, contactPersonName, emailAddress, description, package1);
+        if (result == 1) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Company Registered!", "Please wait for our system administrator to contact you."));
+
+        }
+    }
+
+  
+//    <editor-fold defaultstate="collapsed" desc="getters and setters">
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyAddress() {
+        return companyAddress;
+    }
+
+    public void setCompanyAddress(String companyAddress) {
+        this.companyAddress = companyAddress;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getContactPersonName() {
+        return contactPersonName;
+    }
+
+    public void setContactPersonName(String contactPersonName) {
+        this.contactPersonName = contactPersonName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPackage1() {
+        return package1;
+    }
+
+    public void setPackage1(Integer package1) {
+        this.package1 = package1;
+    }
+
+    public String getFirstNameNewUser() {
+        return firstNameNewUser;
+    }
+
+    public void setFirstNameNewUser(String firstNameNewUser) {
+        this.firstNameNewUser = firstNameNewUser;
+    }
+
+    public String getLastNameNewUser() {
+        return lastNameNewUser;
+    }
+
+    public void setLastNameNewUser(String lastNameNewUser) {
+        this.lastNameNewUser = lastNameNewUser;
+    }
+
+    public String getEmailAddressNewUser() {
+        return emailAddressNewUser;
+    }
+
+    public void setEmailAddressNewUser(String emailAddressNewUser) {
+        this.emailAddressNewUser = emailAddressNewUser;
+    }
+
+    public String getPasswordNewUser() {
+        return passwordNewUser;
+    }
+
+    public void setPasswordNewUser(String passwordNewUser) {
+        this.passwordNewUser = passwordNewUser;
+    }
+
+    public String getPostalAddressNewUser() {
+        return postalAddressNewUser;
+    }
+
+    public void setPostalAddressNewUser(String postalAddressNewUser) {
+        this.postalAddressNewUser = postalAddressNewUser;
+    }
+
+    public String getContactNumberNewUser() {
+        return contactNumberNewUser;
+    }
+
+    public void setContactNumberNewUser(String contactNumberNewUser) {
+        this.contactNumberNewUser = contactNumberNewUser;
+    }
+
+    public String getSalutionNewUser() {
+        return salutionNewUser;
+    }
+
+    public void setSalutionNewUser(String salutionNewUser) {
+        this.salutionNewUser = salutionNewUser;
+    }
+
+    public String getUserTypeNewUser() {
+        return userTypeNewUser;
+    }
+
+    public void setUserTypeNewUser(String userTypeNewUser) {
+        this.userTypeNewUser = userTypeNewUser;
+    }
+
+    public Integer getCompanyIdNewUser() {
+        return companyIdNewUser;
+    }
+
+    public void setCompanyIdNewUser(Integer companyIdNewUser) {
+        this.companyIdNewUser = companyIdNewUser;
+    }
+
+    
+//</editor-fold>
+}
--- MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/UserRoleManagerBean.java
+++ MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/UserRoleManagerBean.java
@@ -5,61 +5,32 @@
  */
 package merlionportal.managedbean;
 
-import entity.SystemUser;
+import entity.UserRole;
+import java.util.List;
 import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
-import javax.faces.view.ViewScoped;
-import javax.inject.Named;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import merlionportal.ci.administrationmodule.CheckAccessRightBean;
-import merlionportal.ci.administrationmodule.UserAccountManagementBean;
-import util.accessRightControl.Right;
+import merlionportal.ci.administrationmodule.RoleManagementBean;
 
 /**
  *
  * @author manliqi
  */
-@Named(value = "userAccountManagedBean")
-@ViewScoped
-public class UserAccountManagedBean {
+@Named(value = "userRoleManagerBean")
+@Dependent
+public class UserRoleManagerBean {
 
     /**
-     * Creates a new instance of UserAccountManagedBean
+     * Creates a new instance of UserRoleManagerBean
      */
     @EJB
-    private UserAccountManagementBean uamb;
-
-    @EJB
-    private CheckAccessRightBean carb;
-
-    private FacesContext ctx;
-
-    @PersistenceContext
-    EntityManager em;
-
-    private Integer userId;
-    private Integer companyId;
-    private String companyName;
-    private String companyAddress;
-    private String contactNumber;
-    private String contactPersonName;
-    private String emailAddress;
-    private String description;
-    private Integer package1;
-
-    private String firstNameNewUser;
-    private String lastNameNewUser;
-    private String emailAddressNewUser;
-    private String passwordNewUser;
-    private String postalAddressNewUser;
-    private String contactNumberNewUser;
-    private String salutionNewUser;
-    private String userTypeNewUser;
-    private Integer companyIdNewUser;
-
+    private RoleManagementBean rmb;
+    
+    private Integer userCompanyId;
+    
     private String roleName;
     private String roleDescription;
     private boolean canGeneratePO;
@@ -90,205 +61,92 @@ public class UserAccountManagedBean {
     private boolean canManageBid;
     private boolean canManagePost;
 
-    public UserAccountManagedBean() {
-    }
-
-    public void createSystemAdmin(ActionEvent event) {
-
-        Integer operatorId = (Integer) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userId");
-        SystemUser operator = em.find(SystemUser.class, operatorId);
-        if (operator != null) {
-            if (carb.userHasRight(operator, Right.canManageUser)) {
-                int result = uamb.createCompanySystemAdminUser(firstNameNewUser, lastNameNewUser, emailAddressNewUser, passwordNewUser, postalAddressNewUser, contactNumberNewUser, salutionNewUser, userTypeNewUser, companyIdNewUser);
-            } else {
-                System.out.println("Access Denied");
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Access Denied!", "You do not have sufficient right to perform this action!"));
-            }
-
-        }
-
+    private List<UserRole> roles;
+    
+    public UserRoleManagerBean() {
+        canGeneratePO = false;
+        canGenerateSO = false;
+        canGenerateQuotationAndProductContract = false;
+        canGenerateSalesReport = false;
+        canManageUser = false;
+        canUseForecast = false;
+        canManageProductAndComponent = false;
+        canGenerateMRPList = false;
+        canGenerateServicePO = false;
+        canUpdateCustomerCredit = false;
+        canGenerateServiceSO = false;
+        canGenerateQuotationRequest = false;
+        canManageServiceCatalog = false;
+        canGenerateServiceQuotationAndContract = false;
+        canManageKeyAccount = false;
+        canManageTransportationAsset = false;
+        canManageTransportationOrder = false;
+        canManageLocation = false;
+        canManageAssetType = false;
+        canUseHRFunction = false;
+        canManageWarehouse = false;
+        canManageStockAuditProcess = false;
+        canManageStockTransportOrder = false;
+        canManageReceivingGoods = false;
+        canManageOrderFulfillment = false;
+        canManageBid = false;
+        canManagePost = false;
     }
 
     public void createSystemAdminRole(ActionEvent event) {
-        Integer operatorId = (Integer) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userId");
-        SystemUser operator = em.find(SystemUser.class, operatorId);
+//        Integer operatorId = (Integer) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userId");
+        Integer operatorId = 2;
+        int result = rmb.createSystemAdminRole(operatorId, userCompanyId);
 
-        if (operator != null) {
-            if (carb.userHasRight(operator, Right.canManageUser)) {
-                int result = uamb.createSystemAdminRole(companyId);
-            } else {
-                System.out.println("Access Denied");
-                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Access Denied."));
-            }
+        if (result == 1) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "System Admin Role created for this company!", ""));
+        } else if (result == -1) {
+
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Access Denied!", "You do not have sufficient right to perform this action!"));
+        } else {
+            //redirect to login page
 
         }
-    }
-
-    public void registerNewCompany(ActionEvent event) {
-
-        int result = uamb.registerNewCompany(companyName, emailAddress, contactNumber, contactPersonName, emailAddress, description, package1);
-
     }
 
     public void createRole(ActionEvent event) {
-        Integer operatorId = (Integer) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userId");
-        SystemUser operator = em.find(SystemUser.class, operatorId);
+        //Integer operatorId = (Integer) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userId");
+        //Integer company = (Integer) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("companyId");
 
-        if (operator != null) {
-            if (carb.userHasRight(operator, Right.canManageUser)) {
-                Integer company = (Integer) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userId");
-                int result = uamb.createCompanyRole(company, roleName, description, canGeneratePO, canGenerateSO, canGenerateQuotationAndProductContract, canGenerateSalesReport, canManageUser, canUseForecast, canManageProductAndComponent, canGenerateMRPList, canGenerateServicePO, canUpdateCustomerCredit, canGenerateServiceSO, canGenerateQuotationRequest, canManageServiceCatalog, canGenerateServiceQuotationAndContract, canManageKeyAccount, canManageTransportationAsset, canManageTransportationOrder, canManageLocation, canManageAssetType, canUseHRFunction, canManageWarehouse, canManageStockAuditProcess, canManageStockTransportOrder, canManageReceivingGoods, canManageOrderFulfillment, canManageBid, canManagePost);
-            }
-                
-        }
-        else{
+        Integer operatorId = 3;
+        Integer companyId = 3;
+        int result = rmb.createCompanyRole(operatorId, companyId, roleName, roleDescription, canGeneratePO, canGenerateSO, canGenerateQuotationAndProductContract, canGenerateSalesReport, canManageUser, canUseForecast, canManageProductAndComponent, canGenerateMRPList, canGenerateServicePO, canUpdateCustomerCredit, canGenerateServiceSO, canGenerateQuotationRequest, canManageServiceCatalog, canGenerateServiceQuotationAndContract, canManageKeyAccount, canManageTransportationAsset, canManageTransportationOrder, canManageLocation, canManageAssetType, canUseHRFunction, canManageWarehouse, canManageStockAuditProcess, canManageStockTransportOrder, canManageReceivingGoods, canManageOrderFulfillment, canManageBid, canManagePost);
+        if (result == 1) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "New Role Created!", "You can now assign users to this role"));
+
+        } else if (result == -1) {
+
             System.out.println("Access Denied");
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Access Denied."));
-        }    
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Access Denied!", "You do not have sufficient right to perform this action!"));
+
+        } else {
+//      redirect to login page
+        }
+
+    }
+     public List<UserRole> getRoles(){
+        //Integer operatorId = (Integer) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userId");
+        //Integer company = (Integer) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("companyId");
+        Integer operatorId = 3;
+        Integer companyId = 2;
+        
+        return rmb.viewAllRoles(operatorId, companyId);
+  
+    }
+     
+
+    //    <editor-fold defaultstate="collapsed" desc="getters and setters">
+    public Integer getUserCompanyId() {
+        return userCompanyId;
     }
 
-    public Integer getCompanyId() {
-        return companyId;
-    }
-
-    public void setCompanyId(Integer companyId) {
-        this.companyId = companyId;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getCompanyAddress() {
-        return companyAddress;
-    }
-
-    public void setCompanyAddress(String companyAddress) {
-        this.companyAddress = companyAddress;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-
-    public String getContactPersonName() {
-        return contactPersonName;
-    }
-
-    public void setContactPersonName(String contactPersonName) {
-        this.contactPersonName = contactPersonName;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getPackage1() {
-        return package1;
-    }
-
-    public void setPackage1(Integer package1) {
-        this.package1 = package1;
-    }
-
-    public String getFirstNameNewUser() {
-        return firstNameNewUser;
-    }
-
-    public void setFirstNameNewUser(String firstNameNewUser) {
-        this.firstNameNewUser = firstNameNewUser;
-    }
-
-    public String getLastNameNewUser() {
-        return lastNameNewUser;
-    }
-
-    public void setLastNameNewUser(String lastNameNewUser) {
-        this.lastNameNewUser = lastNameNewUser;
-    }
-
-    public String getEmailAddressNewUser() {
-        return emailAddressNewUser;
-    }
-
-    public void setEmailAddressNewUser(String emailAddressNewUser) {
-        this.emailAddressNewUser = emailAddressNewUser;
-    }
-
-    public String getPasswordNewUser() {
-        return passwordNewUser;
-    }
-
-    public void setPasswordNewUser(String passwordNewUser) {
-        this.passwordNewUser = passwordNewUser;
-    }
-
-    public String getPostalAddressNewUser() {
-        return postalAddressNewUser;
-    }
-
-    public void setPostalAddressNewUser(String postalAddressNewUser) {
-        this.postalAddressNewUser = postalAddressNewUser;
-    }
-
-    public String getContactNumberNewUser() {
-        return contactNumberNewUser;
-    }
-
-    public void setContactNumberNewUser(String contactNumberNewUser) {
-        this.contactNumberNewUser = contactNumberNewUser;
-    }
-
-    public String getSalutionNewUser() {
-        return salutionNewUser;
-    }
-
-    public void setSalutionNewUser(String salutionNewUser) {
-        this.salutionNewUser = salutionNewUser;
-    }
-
-    public String getUserTypeNewUser() {
-        return userTypeNewUser;
-    }
-
-    public void setUserTypeNewUser(String userTypeNewUser) {
-        this.userTypeNewUser = userTypeNewUser;
-    }
-
-    public Integer getCompanyIdNewUser() {
-        return companyIdNewUser;
-    }
-
-    public void setCompanyIdNewUser(Integer companyIdNewUser) {
-        this.companyIdNewUser = companyIdNewUser;
+    public void setUserCompanyId(Integer userCompanyId) {
+        this.userCompanyId = userCompanyId;
     }
 
     public String getRoleName() {
@@ -522,5 +380,6 @@ public class UserAccountManagedBean {
     public void setCanManagePost(boolean canManagePost) {
         this.canManagePost = canManagePost;
     }
+     //</editor-fold>
 
 }
