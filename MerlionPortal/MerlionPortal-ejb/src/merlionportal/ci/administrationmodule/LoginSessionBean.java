290927882d84ab21257b7b123bc7b668aaa0c85b
xiaolongba0 <xiaolongba0@users.noreply.github.com>
2014-09-11 13:07:23 +0800

    Changed em.find method to look for email address - manli

--- MerlionPortal/MerlionPortal-ejb/src/merlionportal/ci/administrationmodule/LoginSessionBean.java
+++ MerlionPortal/MerlionPortal-ejb/src/merlionportal/ci/administrationmodule/LoginSessionBean.java
@@ -11,6 +11,7 @@ import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 @Stateless
 @LocalBean
@@ -20,13 +21,18 @@ public class LoginSessionBean {
     EntityManager em;
     private int tryouts = 0;
 
+
     public HashMap<String, Integer> verifyAccount(String username, String password) {
-        SystemUser user = em.find(SystemUser.class, username);
+        Query q = em.createNamedQuery("SystemUser.findByEmailAddress").setParameter("emailAddress", username);
+        SystemUser user = (SystemUser) q.getSingleResult();
+        
         HashMap<String, Integer> sessionMap = new HashMap<String, Integer>();
+
         //       if (user != null && lockAccount != true) {
         if (user != null) {
             String corrPassword = user.getPassword();
             if (password.equals(corrPassword)) {
+
                 sessionMap.put("userId", user.getSystemUserId());
                 sessionMap.put("companyId", user.getCompanycompanyId().getCompanyId());
                 return sessionMap;
