46dd9610ba8244f94eb1cdd4dc9f95fb0234de89
xiaolongba0 <xiaolongba0@users.noreply.github.com>
2014-09-12 17:13:27 +0800

    moved bean to .ci - Manli

--- MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/ci/LoginBean.java
+++ MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/ci/LoginBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package merlionportal.managedbean;
+package merlionportal.managedbean.ci;
 
 import java.io.IOException;
 import merlionportal.utility.MD5Generator;
