e3b879029ca1e6c6899e134d398894f694fdaa5a
fyao <smirkyao@hotmail.com>
2014-09-15 17:27:48 +0800

    modified addProduct-Fuyao

--- MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/ProductManagerBean.java
+++ MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/ProductManagerBean.java
@@ -4,10 +4,11 @@ import entity.Product;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+
 import javax.faces.event.ActionEvent;
 import merlionportal.mrp.productcatalogmodule.ProductSessionBean;
 
-//import util.exception.VenueConflictException;
+
 @Named(value = "productManagerBean")
 @RequestScoped
 public class ProductManagerBean {
@@ -22,14 +23,21 @@ public class ProductManagerBean {
     private Integer companyId = 12345;
     private Double productId;
     private Product productTemp;
+    
+  /*  private String componentName;
+    private String componentDescription;
+    private Integer componentQuantity;
+    private String componentCurrency;
+    private Double componentCost;
+    private Integer componentLeadTime;*/
 
-   // private String venue;
-    // private String systemUser;
+
     private String productNameLength;
     private String statusMessage;
     private Integer newProductId;
+ //   private Integer newComponentId;
     
-    private String types = "Manufacturing" + "Non-manufacturing";
+    //private String types = "Manufacturing" + "Non-manufacturing";
 
     public ProductManagerBean() {
         productNameLength = "Current product name length is less than 4.";
@@ -83,29 +91,64 @@ public class ProductManagerBean {
         this.price = price;
     }
     
-     public String getTypes() {
+ /*  Used for 
+    public String getTypes() {
         return types;
     }
 
     public void setTypes(String types) {
         this.types = types;
+    }*/
+    
+    /*    public String getComponentName() {
+        return componentName;
+    }
+
+    public void setComponentName(String componentName) {
+        this.componentName = componentName;
     }
     
+        public String getComponentDescription(){
+        return componentDescription;
+    }
 
-    /*   public String getVenue() {
-     return venue;
-     }
+    public void setComponentDescription(String componentDescription) {
+        this.componentDescription = componentDescription;
+    }
+    
+        public Integer getComponentQuantity() {
+        return componentQuantity;
+    }
 
-     public void setVenue(String venue) {
-     this.venue = venue;
-     }
+    public void setComponentQuantity(Integer componentQuantity) {
+        this.componentQuantity = componentQuantity;
+    }
+    
+     public String getComponentCurrency() {
+        return componentCurrency;
+    }
 
-     public String getSystemUser() {
-     return systemUser;
-     }*/
- //   public void setSystemUser(String systemUser) {
-    //       this.systemUser = systemUser;
-    //  }
+    public void setComponentCurrency(String componentCurrency) {
+        this.componentCurrency = componentCurrency;
+    }
+    
+       public Double getComponentCost() {
+        return componentCost;
+    }
+
+    public void setComponentCost(Double componentCost) {
+        this.componentCost = componentCost;
+    }
+    
+       public Integer getComponentLeadTime() {
+        return componentLeadTime;
+    }
+
+    public void setComponentLeadTime(Integer componentLeadTime) {
+        this.componentLeadTime = componentLeadTime;
+    }
+    
+    */
     public void setProductNameLength(String productNameLength) {
         this.productNameLength = productNameLength;
     }
@@ -148,7 +191,7 @@ public class ProductManagerBean {
         //  Integer systemUserId = Integer.valueOf(systemUser);
         try {
             //companyId =(Integer)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("companyId");
-            newProductId = productSessionBean.addNewProduct(productName, description, productType, currency, price, companyId /*, venueId, systemUserId*/);
+            newProductId = productSessionBean.addNewProduct(productName, description, productType, currency, price, companyId);
             statusMessage = "New Product Saved Successfully";
      //   } catch (VenueConflictException vex) {
             //      statusMessage = "Venue Conflict Exception";
@@ -157,6 +200,26 @@ public class ProductManagerBean {
             ex.printStackTrace();
         }
     }
+    
+    /* do it later
+        public void saveNewComponent(ActionEvent product) {
+
+      //  Integer venueId = Integer.valueOf(venue);
+        //  Integer systemUserId = Integer.valueOf(systemUser);
+        try {
+            //companyId =(Integer)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("companyId");
+        
+       
+            newComponentId = productSessionBean.addNewComponent(componentName, componentDescription, componentQuantity, componentCurrency, componentCost, componentLeadTime, companyId, newProductId);
+            statusMessage = "New Component Saved Successfully";
+     //   } catch (VenueConflictException vex) {
+            //      statusMessage = "Venue Conflict Exception";
+            //       newProductId = -1L;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    */
 
     /*  ////////////////////////////////////////////////////////////////
      public Product getAProduct() {
@@ -182,15 +245,15 @@ public class ProductManagerBean {
     }
     
         ////////////////////////////////////////////////////////////////editing
-        public void editProduct(ActionEvent product) {
+  /*      public void editProduct(ActionEvent product) {
         try {
             int pdtID = productId.intValue();
             productSessionBean.editProduct(productName, description, productType, currency, price, companyId, pdtID);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-    }
-        
+    }  */
+    
 
     public void doAjaxCountProductNameLength() {
         if (productName != null) {
