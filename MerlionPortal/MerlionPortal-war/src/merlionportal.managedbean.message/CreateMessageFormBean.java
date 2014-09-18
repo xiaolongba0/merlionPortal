b35da613bc0a3116cea0cf2dec5bb4847bda6c11
xiaolongba0 <xiaolongba0@users.noreply.github.com>
2014-09-18 11:43:20 +0800

    Changed to named annotation for message bean-  Manli

--- MerlionPortal/MerlionPortal-war/src/merlionportal.managedbean.message/CreateMessageFormBean.java
+++ MerlionPortal/MerlionPortal-war/src/merlionportal.managedbean.message/CreateMessageFormBean.java
@@ -2,11 +2,11 @@ package merlionportal.managedbean;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
 import merlionportal.ci.messagingmodule.MessageSessionBean;
 
-@ManagedBean
+@Named(value="messageFormBean")
 @RequestScoped
 public class CreateMessageFormBean {
 
