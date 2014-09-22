79ec0c484b73aab8622e7eac341a72efa3625a22
fyao <smirkyao@hotmail.com>
2014-09-22 22:16:16 +0800

    rearrange pages and formatting -Fuyao

--- MerlionPortal/MerlionPortal-ejb/src/merlionportal/mrp/productcatalogmodule/ProductSessionBean.java
+++ MerlionPortal/MerlionPortal-ejb/src/merlionportal/mrp/productcatalogmodule/ProductSessionBean.java
@@ -18,35 +18,18 @@ public class ProductSessionBean {
     @PersistenceContext
     private EntityManager entityManager;
     private ArrayList<Component> componentList; //componentList refers to BOM of a product
-    //  private ArrayList<Component> components; //components refers to list of components that a supplier offers
     private Product product;
 
-    /*   public List<Venue> getAllVenues() {
-     Query query = entityManager.createQuery("SELECT v FROM Venue v");
-     return query.getResultList();
-     }
-
-     // private Venue getVenue(Long venueId) {
-     //    Venue venue = entityManager.find(Venue.class, venueId);
-     //     return venue;
-     // }
-
-     private SystemUser getSystemUser(Long systemUserId) {
-     SystemUser systemUser = entityManager.find(SystemUser.class, systemUserId);
-     return systemUser;
-     }*/
-    
-    public Boolean ifCanAdd(String productName){
+    public Boolean ifCanAdd(String productName) {
         Query query = entityManager.createQuery("SELECT p FROM Product p WHERE p.productName = :inProductName");
         query.setParameter("inProductName", productName);
         List<Product> productTemp = query.getResultList();
-        if(productTemp.isEmpty()){
+        if (productTemp.isEmpty()) {
             return true;
         }
-        
         return false;
     }
-    
+
     public List<Product> getAllProducts() {
         Query query = entityManager.createQuery("SELECT e FROM Product e");
         return query.getResultList();
@@ -55,9 +38,10 @@ public class ProductSessionBean {
     // Notes: take in companyId automatically after Login, later change to getMyProducts(Integer companyId)
     public List<Product> getMyProducts(Integer companyId) {
         Query query = entityManager.createQuery("SELECT p FROM Product p WHERE p.companyId = :inCompanyId");
-        query.setParameter("inCompanyId", companyId);
+        query.setParameter("inCompanyId", companyId);      
         return query.getResultList();
-    }
+    }    
+    
 
     // Notes: take in companyId automatically after Login, later change to getMyProducts(Integer companyId)
     public List<Component> getComponentsForAProduct(Integer companyId, Integer productId) {
@@ -98,7 +82,6 @@ public class ProductSessionBean {
 
     public Integer addNewProduct(String productName, String description, String category, String productType, String currency, Double price, Integer companyId) {
 
-        // Company company = new Company(companyId);
         product = new Product();
         product.setProductName(productName);
         product.setDescription(description);
@@ -109,23 +92,15 @@ public class ProductSessionBean {
         product.setCompanyId(companyId);
 
         componentList = new ArrayList<Component>();
-        // product.setComponent(getComponent(componentId));
-        // product.setCompany(company);
-
         product.setComponentList(componentList);
 
         entityManager.persist(product);
         entityManager.flush();
 
-        // company.getProductList().add(product);
-        //  entityManager.merge(company);
         return product.getProductId();
 
     }
 
-    ////when adding a component, adding in supplier's details too
-    //After suppliers account can be set up. this function searches through supplier's company Id, then set the id as the same as supplier's company Id
-    ////do a actionlistener after product
     public Integer addNewComponent(String componentName, String description, Integer quantity, String currency, Double cost, Integer leadTime, int supplierCompanyId, String contactPerson, String contactNumber, String contactEmail, Integer companyId, Integer productId) {
 
         Query query = entityManager.createQuery("SELECT p FROM Product p WHERE p.companyId = :inCompanyId");
@@ -147,13 +122,11 @@ public class ProductSessionBean {
         component.setLeadTime(leadTime);
 
         component.setSupplierCompanyId(supplierCompanyId);
-
         component.setSupplierContactPerson(contactPerson);
         component.setSupplierContactEmail(contactNumber);
         component.setSupplierContactNumber(contactEmail);
         component.setProductproductId(pdt);
 
-        ///check later (19 sep)
         entityManager.persist(component);
         entityManager.flush();
 
@@ -165,7 +138,7 @@ public class ProductSessionBean {
 
     }
 
-    public Integer editProduct(String productName, String description, String category, String productType, String currency, Double price, Integer companyID, Integer productID/*, Integer companyId, Integer componentId*/) {
+    public Integer editProduct(String productName, String description, String category, String productType, String currency, Double price, Integer companyID, Integer productID) {
 
         Query query = entityManager.createQuery("SELECT p FROM Product p WHERE p.companyId = :inCompanyId");
         query.setParameter("inCompanyId", companyID);
@@ -184,7 +157,6 @@ public class ProductSessionBean {
         pdt.setCurrency(currency);
         pdt.setPrice(price);
 
-  
         entityManager.merge(pdt);
         entityManager.flush();
 
@@ -192,8 +164,8 @@ public class ProductSessionBean {
 
     }
 
-    ////Editting
-    public Integer editComponent(String componentName, String description, Double cost, String currency, Integer quantity, Integer leadTime, int supplierCompanyId, String contactPerson, String contactNumber, String contactEmail,Integer companyID, Integer productID, Integer componentID) {
+    //Edit a component
+    public Integer editComponent(String componentName, String description, Double cost, String currency, Integer quantity, Integer leadTime, int supplierCompanyId, String contactPerson, String contactNumber, String contactEmail, Integer companyID, Integer productID, Integer componentID) {
 
         Query query = entityManager.createQuery("SELECT p FROM Product p WHERE p.companyId = :inCompanyId");
         query.setParameter("inCompanyId", companyID);
@@ -213,27 +185,17 @@ public class ProductSessionBean {
         component.setCurrency(currency);
         component.setQuantity(quantity);
         component.setLeadTime(leadTime);
-      
+
         component.setSupplierCompanyId(supplierCompanyId);
 
         component.setSupplierContactPerson(contactPerson);
         component.setSupplierContactEmail(contactNumber);
         component.setSupplierContactNumber(contactEmail);
-        
-        /*   component.getSuppliersupplierCompanyId().setContactPerson(contactPerson);
-         component.getSuppliersupplierCompanyId().setContactNumber(contactNumber);
-         component.getSuppliersupplierCompanyId().setDescription(supplierDescription);
-         component.getSuppliersupplierCompanyId().setContactEmail(contactEmail);*/
 
-        // product.setComponent(getComponent(componentId));
-        // product.setCompany(company);
         entityManager.merge(component);
         entityManager.flush();
 
-        // company.getProductList().add(product);
-        //  entityManager.merge(company);
         return component.getComponentId();
-
     }
 
     public void deleteProducts(Integer companyId, Double productId) {
