7db60016975713d0ec9d700d8a3dad6e2b4260f6
Julietta <JuliettaLee@users.noreply.github.com>
2014-09-23 16:58:49 +0800

    dashboard hover javascript

--- MerlionPortal/MerlionPortal-war/web/js/dashboard.js
+++ MerlionPortal/MerlionPortal-war/web/js/dashboard.js
@@ -1,2 +1,9 @@
-
+$(document).ready(function() {
+    $('.dashboard-box').mouseenter(function() {
+        $(this).addClass('highlight');
+    });
+    $('.dashboard-box').mouseleave(function() {
+    $(this).removeClass('highlight');
+    });
+});
 
