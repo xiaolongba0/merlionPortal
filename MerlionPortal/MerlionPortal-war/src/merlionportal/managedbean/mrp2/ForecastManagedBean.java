8da1c1229ed05327ee40883ba894c6310d0a61f4
fyao <smirkyao@hotmail.com>
2014-09-27 17:53:15 +0800

    integrate nav bar-Fuyao

--- MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/mrp2/ForecastManagedBean.java
+++ MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/mrp2/ForecastManagedBean.java
@@ -95,6 +95,10 @@ public class ForecastManagedBean implements Serializable {
         return ("mps");
     }
     
+    public String backToHistory() {
+        return ("forecastviewhistory");
+    }
+    
     
 
 }
