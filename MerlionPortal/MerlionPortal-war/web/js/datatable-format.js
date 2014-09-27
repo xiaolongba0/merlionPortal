9f9420fe3388cc43759683f17869e11dd79717ed
Julietta <JuliettaLee@users.noreply.github.com>
2014-09-27 18:09:52 +0800

    javascript for formatting datatable

---
+++ MerlionPortal/MerlionPortal-war/web/js/datatable-format.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("div div select").after("<span>items per page</span>");
+});
+
+$(document).ready(function(){
+    $("form div div select").before("<span>    Display</span>");
+});
