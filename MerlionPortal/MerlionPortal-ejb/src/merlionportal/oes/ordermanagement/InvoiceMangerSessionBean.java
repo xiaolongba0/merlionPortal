894e8dd53ea82d0b19b71b1bd197eb296ec6a66f
xiamli <xiameng2@gmail.com>
2014-09-28 15:12:22 +0800

    im done DO NOT CHANGE

--- MerlionPortal/MerlionPortal-ejb/src/merlionportal/oes/ordermanagement/InvoiceMangerSessionBean.java
+++ MerlionPortal/MerlionPortal-ejb/src/merlionportal/oes/ordermanagement/InvoiceMangerSessionBean.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package merlionportal.oes.ordermanagement;
 
 import entity.ProductInvoice;
@@ -23,6 +22,7 @@ import javax.persistence.Query;
 @Stateless
 @LocalBean
 public class InvoiceMangerSessionBean {
+
     @PersistenceContext(unitName = "MerlionPortal-ejbPU")
     private EntityManager em;
 
@@ -33,27 +33,73 @@ public class InvoiceMangerSessionBean {
         em.persist(object);
     }
 
-  public List<ProductOrder> getAllWaitingForInvoice(int companyId){
-      List<ProductOrder> result= new ArrayList();
-      Query q=em.createQuery("SELECT p FROM ProductOrder p WHERE p.companyId = :companyId AND p.status =:status");
-      q.setParameter("companyId", companyId);
-      q.setParameter("status", 2);
-      for(Object o: q.getResultList()){
-          ProductOrder order=(ProductOrder) o;
-          result.add(order);
-      }
-      return result;
-  }
-  
-  public void generateInvoice(ProductOrder myOrder,int orderId, Double totalPrice, int customerId){
-      ProductInvoice newInvoice = new ProductInvoice();
-      newInvoice.setCustomerId(customerId);
-      newInvoice.setSalesOrderId(orderId);
-      newInvoice.setTotalPrice(totalPrice);
-      newInvoice.setStatus("Invoiced");
-      em.persist(newInvoice);
-      myOrder.setStatus(5);
-      em.merge(myOrder);
-      
-  }
+    public List<ProductOrder> getAllWaitingForInvoice(int companyId) {
+        List<ProductOrder> result = new ArrayList();
+        Query q = em.createQuery("SELECT p FROM ProductOrder p WHERE p.companyId = :companyId AND p.status =:status");
+        q.setParameter("companyId", companyId);
+        q.setParameter("status", 2);
+        for (Object o : q.getResultList()) {
+            ProductOrder order = (ProductOrder) o;
+            result.add(order);
+        }
+        return result;
+    }
+
+    public void generateInvoice(ProductOrder myOrder, int orderId, Double totalPrice, int customerId, String text) {
+        ProductInvoice newInvoice = new ProductInvoice();
+        newInvoice.setCustomerId(customerId);
+        newInvoice.setSalesOrderId(orderId);
+        newInvoice.setTotalPrice(totalPrice);
+        newInvoice.setStatus("Invoiced");
+        newInvoice.setCondition(text);
+        em.persist(newInvoice);
+        myOrder.setStatus(5);
+        em.merge(myOrder);
+
+    }
+
+    public List<ProductInvoice> viewAllInvoice(int companyId) {
+        Query q = em.createQuery("SELECT p FROM ProductInvoice p");
+        int salesOrderId;
+        List<ProductInvoice> result = new ArrayList();
+        for (Object o : q.getResultList()) {
+            ProductInvoice invoice = (ProductInvoice) o;
+            salesOrderId = invoice.getSalesOrderId();
+            ProductOrder myOrder = em.find(ProductOrder.class, salesOrderId);
+            if (companyId == myOrder.getCompanyId()) {
+                result.add(invoice);
+            }
+
+        }
+        return result;
+
+    }
+
+    public List<ProductInvoice> viewAllInvoice(int companyId, int userId) {
+        Query q = em.createQuery("SELECT p FROM ProductInvoice p WHERE p.customerId = :customerId").setParameter("customerId", userId);
+        int salesOrderId;
+        List<ProductInvoice> result = new ArrayList();
+        for (Object o : q.getResultList()) {
+            ProductInvoice invoice = (ProductInvoice) o;
+            salesOrderId = invoice.getSalesOrderId();
+            ProductOrder myOrder = em.find(ProductOrder.class, salesOrderId);
+            if (companyId == myOrder.getCompanyId()) {
+                result.add(invoice);
+            }
+
+        }
+        return result;
+    }
+    
+    public List<String> setAllStatus(){
+        List<String> allStatus =new ArrayList();
+        allStatus.add("Invoiced");
+        allStatus.add("Waiting for credit");
+        return allStatus;
+    }
+    
+    public ProductOrder getMyOrder(int orderId){
+        ProductOrder myOrder = em.find(ProductOrder.class, orderId);
+        return myOrder;
+    }
 }
