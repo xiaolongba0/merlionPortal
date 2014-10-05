c6448df1001e0bf2e26864e722721acff3347f5d
wenxin274 <wenxin274@gmail.com>
2014-10-05 18:05:34 +0800

    updated

---
+++ MerlionPortal/MerlionPortal-ejb/src/merlionportal/ci/messagingmodule/FeedbackSessionBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package merlionportal.ci.messagingmodule;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author wenxin274
+ */
+@Stateless
+@LocalBean
+public class FeedbackSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
