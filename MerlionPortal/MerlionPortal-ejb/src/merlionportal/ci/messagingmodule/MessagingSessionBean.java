9afc1a47854cd69816fb4c1ad3eaa2ae5ee45701
wenxin274 <wenxin274@gmail.com>
2014-10-09 20:09:00 +0800

    add basic messaging ui (component) add messaging component to admin dashboard add session bean for messaging module add messaging status in utils.
    cheers

---
+++ MerlionPortal/MerlionPortal-ejb/src/merlionportal/ci/messagingmodule/MessagingSessionBean.java
@@ -0,0 +1,73 @@
+package merlionportal.ci.messagingmodule;
+
+import entity.Message;
+import entity.SystemUser;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+@Stateless
+@LocalBean
+public class MessagingSessionBean {
+
+    @PersistenceContext
+    EntityManager em;
+    Query q;
+
+    public int findUser(String email) {
+        q = em.createNamedQuery("SystemUser.findByEmailAddress").setParameter("emailAddress", email);
+        if (q.getResultList().isEmpty()) {
+            return -1;
+        } else {
+            SystemUser su = (SystemUser) q.getResultList().get(0);
+            return su.getSystemUserId();
+        }
+    }
+    
+    public String findEmail(int id){
+        SystemUser su = em.find(SystemUser.class, id);
+        if(su != null){
+            return su.getEmailAddress();
+        }else{
+            return "";
+        }
+    }
+
+    //Return message id upon successful creation.
+    public int createMessage(int senderId, int receiverId, String title, String body, int status) {
+        int response = -1;
+        try{
+        Message msg = new Message();
+        msg.setMessageTitle(title);
+        msg.setMessageBody(body);
+        msg.setSender(senderId);
+        msg.setReceiver(receiverId);
+        msg.setSentTime(new Date());
+        msg.setStatus(status);
+        msg.setMessageType("mail");
+        em.persist(msg);
+        em.flush();
+        em.refresh(msg);
+        response = msg.getMessageId();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    public List<Message> getInbox(int customerId) {
+        List<Message> inbox;
+        q = em.createNamedQuery("Message.findByReceiver").setParameter("receiver", customerId);
+        if (q.getResultList().isEmpty()) {
+            inbox = new ArrayList<Message>();
+        } else {
+            inbox = q.getResultList();
+        }
+        return inbox;
+    }
+}
---
+++ MerlionPortal/MerlionPortal-war/src/merlionportal/utility/MessagingStatus.java
@@ -0,0 +1,8 @@
+package merlionportal.utility;
+
+public class MessagingStatus {
+    public static final int MAIL_UNREAD = 9990;
+    public static final int MAIL_READ = 9991;
+    public static final int MAIL_DELETED = 9992;
+    
+}
