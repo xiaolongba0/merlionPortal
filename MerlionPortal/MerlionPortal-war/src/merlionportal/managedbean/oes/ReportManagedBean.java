ebb0a1b99259aa2218f81ba7c437a6aac3926a8a
xiamli <xiameng2@gmail.com>
2014-10-11 15:55:24 +0800

    report managed bean updated

--- MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/oes/ReportManagedBean.java
+++ MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/oes/ReportManagedBean.java
@@ -7,7 +7,6 @@ package merlionportal.managedbean.oes;
 
 import entity.ProductOrder;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -39,6 +38,12 @@ public class ReportManagedBean {
     private Date startDate;
     private Date endDate;
     private List<ProductOrder> productOrderList;
+    private Integer customerId;
+    private Date firstStartDate;
+    private Date firstEndDate;
+    private Date secondStartDate;
+    private Date secondEndDate;
+    private List<ProductOrder> secondOrderList;
 
     public ReportManagedBean() {
     }
@@ -63,7 +68,7 @@ public class ReportManagedBean {
         }
     }
 
-    public String allValidOrders() throws ParseException {
+    public String allValidOrders() {
         if (startDate == null || endDate == null) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "!", "Please select starting date and end date"));
             return null;
@@ -78,6 +83,36 @@ public class ReportManagedBean {
             sessionMap.put("OrderList", productOrderList);
         }
         return "reportdisplaysalessum.xhtml?faces-redirect=true";
+    }
+
+    public String allCustomerOrders() {
+        if (firstStartDate == null||secondStartDate==null) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "!", "Please specify Starting date"));
+            return null;
+
+        } else if (firstEndDate == null||secondEndDate==null) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "!", "Please specify End date"));
+            return null;
+
+        } else if (customerId == null) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "!", "Please specify Customer Id "));
+            return null;
+
+        } else {
+            System.out.println(startDate + " " + endDate + " " + customerId);
+            productOrderList = reportMB.findAllProduct(companyId, firstStartDate, firstEndDate, customerId);
+            secondOrderList= reportMB.findAllProduct(companyId, secondStartDate, secondEndDate, customerId);
+            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+            Map<String, Object> sessionMap = externalContext.getSessionMap();
+            sessionMap.put("FirstOrderList", productOrderList);
+            sessionMap.put("SecondOrderList", secondOrderList);
+            sessionMap.put("firstStartDate", firstStartDate);
+            sessionMap.put("firstEndDate", firstEndDate);
+            sessionMap.put("secondStartDate", secondStartDate);
+            sessionMap.put("secondEndDate", secondEndDate);
+            sessionMap.put("reportCustomer", customerId);
+        }
+        return "customeranalysisdetail.xhtml?faces-redirect=true";
     }
 
     public Date getStartDate() {
@@ -118,6 +153,46 @@ public class ReportManagedBean {
 
     public void setUserId(Integer userId) {
         this.userId = userId;
+    }
+
+    public Date getFirstStartDate() {
+        return firstStartDate;
+    }
+
+    public void setFirstStartDate(Date firstStartDate) {
+        this.firstStartDate = firstStartDate;
+    }
+
+    public Date getFirstEndDate() {
+        return firstEndDate;
+    }
+
+    public void setFirstEndDate(Date firstEndDate) {
+        this.firstEndDate = firstEndDate;
+    }
+
+    public Date getSecondStartDate() {
+        return secondStartDate;
+    }
+
+    public void setSecondStartDate(Date secondStartDate) {
+        this.secondStartDate = secondStartDate;
+    }
+
+    public Date getSecondEndDate() {
+        return secondEndDate;
+    }
+
+    public void setSecondEndDate(Date secondEndDate) {
+        this.secondEndDate = secondEndDate;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
     }
 
 }
