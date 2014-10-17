30f809ff2bf9a4dd763b0d7685a74100b4381809
xiaolongba0 <xiaolongba0@users.noreply.github.com>
2014-10-17 22:27:31 +0800

    Entity Modification for 1-1 relationship -- Manli

--- MerlionPortal/MerlionPortal-ejb/src/entity/ServiceInvoice.java
+++ MerlionPortal/MerlionPortal-ejb/src/entity/ServiceInvoice.java
@@ -12,6 +12,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
@@ -20,7 +22,6 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -44,8 +45,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class ServiceInvoice implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
-    @NotNull
     @Column(name = "invoiceId")
     private Integer invoiceId;
     // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
