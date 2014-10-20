075dee1be11817ee745d77bbd75138467dd700f3
wenxin274 <wenxin274@gmail.com>
2014-10-21 01:53:28 +0800

    added Feedback functionality
    everyone:
    -view feedback and likes
    -view comments and likes
    
    logined members:
    -post feedback
    -post comment
    -like feedback
    -like comment
    
    limitation: cannot use enter sign, comment title must not have special characters.

--- MerlionPortal/MerlionPortal-ejb/src/merlionportal/ci/messagingmodule/FeedbackSessionBean.java
+++ MerlionPortal/MerlionPortal-ejb/src/merlionportal/ci/messagingmodule/FeedbackSessionBean.java
@@ -1,24 +1,105 @@
 package merlionportal.ci.messagingmodule;
 
+import entity.Comment;
 import entity.FeedBack;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 @LocalBean
 public class FeedbackSessionBean {
 
-    public int createFeedback(int creatorId, String content){
+    @PersistenceContext
+    EntityManager em;
+
+    public int createFeedback(int creatorId, String title, String content) {
         FeedBack fb = new FeedBack();
+        fb.setTitle(title);
+        fb.setContent(content);
+        fb.setPoster(creatorId);
+        fb.setCreateDate(new Date());
+        fb.setLikes(0);
+
+        try {
+            em.persist(fb);
+            em.flush();
+            em.refresh(fb);
+            return fb.getFeedbackId();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public int addLike(int id, int threadType) {
+
+        //Feedback
+        if (threadType == 100) {
+            FeedBack fb = em.find(FeedBack.class, id);
+            if (fb != null) {
+                int likes = (fb.getLikes() == null) ? 0 : fb.getLikes();
+                likes += 1;
+                fb.setLikes(likes);
+                em.merge(fb);
+                em.flush();
+                em.refresh(fb);
+                return fb.getFeedbackId();
+            }
+
+        } //Comments
+        else if (threadType == 200) {
+            Comment c = em.find(Comment.class, id);
+            if (c != null) {
+                int likes = (c.getLikes() == null) ? 0 : c.getLikes();
+                likes += 1;
+                c.setLikes(likes);
+                em.merge(c);
+                em.flush();
+                em.refresh(c);
+                return c.getCommentId();
+            }
+        }
         return -1;
     }
-    
-    public int addLike(int id,int threadType){
+
+    public int createComment(int creatorId, String content, int feedbackId) {
+        FeedBack fb = em.find(FeedBack.class, feedbackId);
+        if (fb != null) {
+            Comment c = new Comment();
+            c.setCommentBody(content);
+            c.setLikes(0);
+            c.setPoster(creatorId);
+            c.setCreateDate(new Date());
+            c.setFeedback(fb);
+            em.persist(c);
+            em.flush();
+            em.refresh(c);
+            return c.getCommentId();
+        }
         return -1;
     }
-    
-    public int createComment(int creatorId, String content, int feedbackId){
-        return -1;
+
+    public List<FeedBack> getFeedBacks() {
+        List<FeedBack> fbList = em.createNamedQuery("FeedBack.findAll").getResultList();
+        List<FeedBack> resList = new ArrayList<>();
+        for (FeedBack fb : fbList) {
+            em.refresh(fb);
+            resList.add(fb);
+        }
+
+        return resList;
     }
-    
+
+    public FeedBack getFeedBack(int id) {
+        return em.find(FeedBack.class, id);
+    }
+
+    public Comment getComment(int id) {
+        return em.find(Comment.class, id);
+    }
 }
--- MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/ci/FeedbackManagedBean.java
+++ MerlionPortal/MerlionPortal-war/src/merlionportal/managedbean/ci/FeedbackManagedBean.java
@@ -1,14 +1,19 @@
 package merlionportal.managedbean.ci;
 
+import entity.Comment;
+import entity.FeedBack;
 import entity.SystemUser;
-import java.io.IOException;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import merlionportal.ci.administrationmodule.UserAccountManagementSessionBean;
 import merlionportal.ci.loggingmodule.SystemLogSessionBean;
+import merlionportal.ci.messagingmodule.FeedbackSessionBean;
+import org.primefaces.context.RequestContext;
 
 @Named(value = "feedbackBean")
 @ViewScoped
@@ -20,32 +25,85 @@ public class FeedbackManagedBean {
     @EJB
     private UserAccountManagementSessionBean uamsb;
 
+    @EJB
+    private FeedbackSessionBean fsb;
+
     private Integer companyId;
     private Integer userId;
     private SystemUser loginedUser;
+    private List<FeedBack> feedbackList;
+    private ExternalContext ec;
+
+    //Form
+    private String feedBackTitle;
+    private String feedBackContent;
+    private String commentContent;
+
+    //Types
+    private int TYPE_FEEDBACK = 100;
+    private int TYPE_COMMENT = 200;
 
     @PostConstruct
     public void init() {
+        ec = FacesContext.getCurrentInstance().getExternalContext();
         userId = null;
         loginedUser = null;
-        if (FacesContext.getCurrentInstance().getExternalContext().getSessionMap().containsKey("userId")) {
+        if (ec.getSessionMap().containsKey("userId")) {
             userId = (int) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userId");
             if (userId > 0) {
                 loginedUser = uamsb.getUser(userId);
             }
         }
+        onLoad();
+    }
+
+    public void onLoad() {
+        this.setFeedbackList(fsb.getFeedBacks());
     }
 
     public void postFeedback() {
-
+        if (fsb.createFeedback(userId, feedBackTitle, feedBackContent) > -1) {
+            RequestContext.getCurrentInstance().execute("postCreateFeedback()");
+        }
+        onLoad();
     }
 
-    public void replyFeedback() {
-
+    public void postComment() {
+        int id = Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("fbid"));
+        if (userId == null) {
+            RequestContext.getCurrentInstance().execute("noUserFound()");
+        } else {
+            String commentMsg = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("commentInput-" + Integer.toString(id));
+            if (fsb.createComment(userId, commentMsg, id) > -1) {
+                RequestContext.getCurrentInstance().execute("commentCreated()");
+                onLoad();
+            }
+        }
     }
 
     public void likeFeedback() {
+        int id = Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("fbid"));
+        if (userId == null) {
+            RequestContext.getCurrentInstance().execute("noUserFound()");
+        } else {
+            if (fsb.addLike(id, TYPE_FEEDBACK) > -1) {
+                FeedBack fb = fsb.getFeedBack(id);
+                RequestContext.getCurrentInstance().execute("feedbackLiked('" + fb.getTitle() + "')");
+                onLoad();
+            }
+        }
+    }
 
+    public void likeComment() {
+        int id = Integer.parseInt(FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("commentid"));
+        if (userId == null) {
+            RequestContext.getCurrentInstance().execute("noUserFound()");
+        } else {
+            if (fsb.addLike(id, TYPE_COMMENT) > -1) {
+                RequestContext.getCurrentInstance().execute("commentLiked()");
+                onLoad();
+            }
+        }
     }
 
     public SystemLogSessionBean getSystemLogSB() {
@@ -78,5 +136,53 @@ public class FeedbackManagedBean {
 
     public void setLoginedUser(SystemUser loginedUser) {
         this.loginedUser = loginedUser;
+    }
+
+    public int getTYPE_FEEDBACK() {
+        return TYPE_FEEDBACK;
+    }
+
+    public void setTYPE_FEEDBACK(int TYPE_FEEDBACK) {
+        this.TYPE_FEEDBACK = TYPE_FEEDBACK;
+    }
+
+    public int getTYPE_COMMENT() {
+        return TYPE_COMMENT;
+    }
+
+    public void setTYPE_COMMENT(int TYPE_COMMENT) {
+        this.TYPE_COMMENT = TYPE_COMMENT;
+    }
+
+    public List<FeedBack> getFeedbackList() {
+        return feedbackList;
+    }
+
+    public void setFeedbackList(List<FeedBack> feedbackList) {
+        this.feedbackList = feedbackList;
+    }
+
+    public String getFeedBackTitle() {
+        return feedBackTitle;
+    }
+
+    public void setFeedBackTitle(String feedBackTitle) {
+        this.feedBackTitle = feedBackTitle;
+    }
+
+    public String getFeedBackContent() {
+        return feedBackContent;
+    }
+
+    public void setFeedBackContent(String feedBackContent) {
+        this.feedBackContent = feedBackContent;
+    }
+
+    public String getCommentContent() {
+        return commentContent;
+    }
+
+    public void setCommentContent(String commentContent) {
+        this.commentContent = commentContent;
     }
 }
