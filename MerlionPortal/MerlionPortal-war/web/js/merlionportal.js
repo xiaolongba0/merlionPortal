3aaedb94a59edc30bd0595b4b4899a22e015ef90
Julietta <JuliettaLee@users.noreply.github.com>
2014-11-03 15:32:04 +0800

    many many edits

--- MerlionPortal/MerlionPortal-war/web/js/merlionportal.js
+++ MerlionPortal/MerlionPortal-war/web/js/merlionportal.js
@@ -1,17 +1,4 @@
 $(document).ready(function(){
-  //shows the menuitems
   $(".ui-panelmenu-content").css("display","block"); 
-  
-  //sets the submenu header to active state
   $(".ui-panelmenu-header").addClass("ui-state-active");
-  
-//  //sets the triangle icon to "expaned" version
-//  $(".ui-icon-triangle-1-e").removeClass("ui-icon-triangle-1-e").addClass("ui-icon-triangle-1-s"); 
 });
-
-//
-////To expand the panel menu only if is collapsed
-//var menuIsExpanded = jQuery('div.ui-panelmenu-content.ui-widget-content.ui-helper-hidden').is(':visible');
-//    if (!menuIsExpanded) {
-//        jQuery('.ui-panelmenu-panel a').first().click();
-//    }
