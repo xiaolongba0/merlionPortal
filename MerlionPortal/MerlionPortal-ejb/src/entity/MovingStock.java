98eb7cafa22a7b327894f7a7ad24a4d90f26a05f
xiaolongba0 <xiaolongba0@users.noreply.github.com>
2014-11-05 15:03:14 +0800

    Moving Stock entity-- Manli

--- MerlionPortal/MerlionPortal-ejb/src/entity/MovingStock.java
+++ MerlionPortal/MerlionPortal-ejb/src/entity/MovingStock.java
@@ -7,6 +7,7 @@
 package entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,6 +19,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -32,7 +36,10 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "MovingStock.findByMovingStockId", query = "SELECT m FROM MovingStock m WHERE m.movingStockId = :movingStockId"),
     @NamedQuery(name = "MovingStock.findByMovingQuantity", query = "SELECT m FROM MovingStock m WHERE m.movingQuantity = :movingQuantity"),
     @NamedQuery(name = "MovingStock.findBySourceStorageBinId", query = "SELECT m FROM MovingStock m WHERE m.sourceStorageBinId = :sourceStorageBinId"),
-    @NamedQuery(name = "MovingStock.findByDestStorageBinId", query = "SELECT m FROM MovingStock m WHERE m.destStorageBinId = :destStorageBinId")})
+    @NamedQuery(name = "MovingStock.findByDestStorageBinId", query = "SELECT m FROM MovingStock m WHERE m.destStorageBinId = :destStorageBinId"),
+    @NamedQuery(name = "MovingStock.findByCreatedDate", query = "SELECT m FROM MovingStock m WHERE m.createdDate = :createdDate"),
+    @NamedQuery(name = "MovingStock.findByStockName", query = "SELECT m FROM MovingStock m WHERE m.stockName = :stockName"),
+    @NamedQuery(name = "MovingStock.findByExpiryDate", query = "SELECT m FROM MovingStock m WHERE m.expiryDate = :expiryDate")})
 public class MovingStock implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
@@ -46,6 +53,15 @@ public class MovingStock implements Serializable {
     private Integer sourceStorageBinId;
     @Column(name = "destStorageBinId")
     private Integer destStorageBinId;
+    @Column(name = "createdDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Size(max = 100)
+    @Column(name = "stockName")
+    private String stockName;
+    @Column(name = "expiryDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date expiryDate;
     @JoinColumn(name = "transportOrder", referencedColumnName = "transportOrderId")
     @ManyToOne(optional = false)
     private TransportOrder transportOrder;
@@ -87,6 +103,30 @@ public class MovingStock implements Serializable {
 
     public void setDestStorageBinId(Integer destStorageBinId) {
         this.destStorageBinId = destStorageBinId;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
     }
 
     public TransportOrder getTransportOrder() {
